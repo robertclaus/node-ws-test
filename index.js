673e6baccabe037c10bfdf8fe999e13d9ab45a3c
Robert Claus <robertclaus@gmail.com>
2019-06-21 02:51:36 -0500

    test

--- index.js
+++ index.js
@@ -16,16 +16,65 @@ var wss = new WebSocketServer({server: server})
 console.log("websocket server created")
 
 
-
+var all_connections = [];
 wss.on("connection", function(ws) {
-  var id = setInterval(function() {
-    ws.send(uuidv1(), function() {  })
-  }, 1000)
+      all_connections.push(ws);
+      ws.id = "unknown";
 
-  console.log("websocket connection open")
+      console.log("websocket connection open");
 
-  ws.on("close", function() {
-    console.log("websocket connection close")
-    clearInterval(id)
-  })
+      ws.on("close", function() {
+          console.log("websocket connection close")
+          for( var i = 0; i < all_connections.length; i++){
+               if ( all_connections[i] === ws) {
+                 all_connections.splice(i, 1);
+               }
+           }
+      });
+
+      ws.on('message', function incoming(data) {
+          console.log(data);
+          message = JSON.parse(data);
+
+          if ( ! "type" in message) {
+             return;
+          }
+
+          if ( message.type == 'whoami') {
+            ws.id = message.iam;
+          }
+
+          if ( message.type == 'addWire' ||
+               message.type == 'removeWire' ||
+               message.type == 'listWires' ||
+               message.type == 'clearWires') {
+            for( var i=0; i<all_connections.length; i++){
+                if (! "destination" in message || ! "receiver" in message) { return;}
+                if (! "device_id" in message.destination || ! "device_id" in message.receiver) { return;}
+
+                if (message.destination.device_id == all_connections[i].id ||
+                    message.receiver.device_id == all_connections[i].id) {
+                    all_connections[i].send(data);
+                }
+
+            }
+          }
+
+          if ( message.type == 'data') {
+            for( var i=0; i<all_connections.length; i++){
+                if (! "destination" in message || ! "receiver" in message) { return;}
+                if (! "device_id" in message.destination || ! "device_id" in message.receiver) { return;}
+
+                if (message.destination.device_id == all_connections[i].id) {
+                    all_connections[i].send(data);
+                }
+            }
+          }
+
+          if ( message.type == 'echo') {
+            for( var i=0; i<all_connections.length; i++){
+                all_connections[i].send(data);
+            }
+          }
+        });
 })

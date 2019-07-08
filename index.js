aab9fae1930c637fd6bcfd76ac986dd821ca216f
Robert Claus <robertclaus@gmail.com>
2019-07-07 22:07:14 -0500

    ifttt device handler

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const ifttt_connection = new WebSocket(url);
 
 var ifttt_data = [];
 
-ifttt_connection.onopen = function() { message = {"type": "whoami", "iam": "I1"}; ws.send(JSON.stringify(message)); }
+ifttt_connection.onopen = function() { console.log("connected"); message = {"type": "whoami", "iam": "I1"}; ifttt_connection.send(JSON.stringify(message)); }
 ifttt_connection.onerror = function(error) { console.log('WebSocket error: ${error}'); }
 ifttt_connection.onmessage = function(e) {
   console.log(e.data);
